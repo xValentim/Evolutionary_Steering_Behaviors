7247260e308e41b8fb8dae60106061dde3d7f178
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-05-04 10:26:09 -0300

    Atualização - lista de imagens

--- values.py
+++ values.py
@@ -1,3 +1,4 @@
+import pygame
 largura = 640
 altura = 320
 
@@ -7,3 +8,25 @@ white = (255, 255, 255)
 red = (250, 0, 0)
 green = (0, 250, 0)
 blue = (0, 0, 250)
+
+vehicle_img = pygame.image.load("Evolutionary_Steering_Behaviors/imgs_vehicles/vehicle_none.png")
+vehicle_img0 = pygame.image.load("Evolutionary_Steering_Behaviors/imgs_vehicles/vehicle_none.png")
+vehicle_img1 = pygame.image.load("Evolutionary_Steering_Behaviors/imgs_vehicles/vehicle_health1.png")
+vehicle_img2 = pygame.image.load("Evolutionary_Steering_Behaviors/imgs_vehicles/vehicle_health2.png")
+vehicle_img3 = pygame.image.load("Evolutionary_Steering_Behaviors/imgs_vehicles/vehicle_health3.png")
+vehicle_img4 = pygame.image.load("Evolutionary_Steering_Behaviors/imgs_vehicles/vehicle_health4.png")
+vehicle_img5 = pygame.image.load("Evolutionary_Steering_Behaviors/imgs_vehicles/vehicle_health5.png")
+vehicle_img6 = pygame.image.load("Evolutionary_Steering_Behaviors/imgs_vehicles/vehicle_health6.png")
+
+vehicles_base = [
+    vehicle_img0,
+    vehicle_img1,
+    vehicle_img2,
+    vehicle_img3,
+    vehicle_img4,
+    vehicle_img5,
+    vehicle_img6,
+]
+
+vehicles = list(vehicles_base)
+
