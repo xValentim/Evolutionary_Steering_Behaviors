7f4b4d02d1dc36f0972b9e5f8b87229f8bfc0b55
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-05-04 00:15:42 -0300

    Atualização - Values

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import pygame
 import numpy
 from vehicle import *
+from values import *
 
 pygame.init()
-largura = 640
-altura = 320
-
-gray = (51, 51, 51)
-black = (0, 0, 0)
-white = (255, 255, 255)
-red = (250, 0, 0)
-green = (0, 250, 0)
-blue = (0, 0, 250)
-
 u = pygame.Vector2()
 v = pygame.Vector2(1, 1)
 
