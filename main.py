0c0d73f9784246769172436a3f149a251863fd3e
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-05-04 10:26:00 -0300

    Atualizações da funcao main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy
+import random
 from vehicle import *
 from values import *
 
@@ -10,11 +11,17 @@ v = pygame.Vector2(1, 1)
 # Dots green : Food
 # Dots red : Poison
 
+
 relogio = pygame.time.Clock()
 window = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Evolutionary Steering Behaviors")
 window.fill(gray)
 v1 = Vehicle(largura / 2, altura / 2)
+food = []
+for i in range(10):
+    x = random.randint(1, largura)
+    y = random.randint(1, altura)
+    food.append(pygame.Vector2(x, y))
 
 continua = True
 while continua:
@@ -25,16 +32,29 @@ while continua:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 continua = False
-        '''if event.type == pygame.MOUSEBUTTONUP:
-            target = pygame.mouse.get_pos()
-            print(target)'''
         target = pygame.mouse.get_pos()
-        #print(pygame.mouse.get_pos())
-    
+        
     v1.seek(target)
     v1.update()
     window.fill(gray)
-    pygame.draw.circle(window, white, v1.position, v1.r)
+    #pygame.draw.circle(window, white, v1.position, v1.r)
+    health_i = 3
+    vehicles[health_i] = pygame.transform.scale(vehicles_base[health_i], (28, 15))
+
+    '''vehicle_img = vehicle_img_temp.get_rect()
+    vehicle_img0 = vehicle_img0_temp.get_rect()'''
+
+    teta = v1.velocity.as_polar()
+    teta = teta[1]
+    
+    vehicles[health_i] = pygame.transform.rotate(vehicles[health_i], -teta)
+    
+    rect = vehicles[health_i].get_rect()
+    rect.center = v1.position
+    
+    window.blit(vehicles[health_i], rect)
+    for i in range(len(food)):
+        pygame.draw.circle(window, green, food[i], 2)
     relogio.tick(60)
     pygame.display.update()
 
